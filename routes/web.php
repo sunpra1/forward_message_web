e417911d9db65cd430c7feb63c7e00f9bab092cc
sunpra <sunpra12@gmail.com>
2024-02-15 01:06:00 +0545

    Stripe key changes.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
 <?php
-
-use App\Http\Controllers\Api\MobileApplicationController;
-use App\Http\Controllers\Api\MemoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('privacy-policy')->group(function () {
